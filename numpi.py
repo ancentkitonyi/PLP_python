8d8e114c35d15c14d2120f1bff56e04b13869932
Ancentdreezy <ancentkitonyi.m@gmail.com>
2024-03-14 11:23:53 +0300

    numpy,pandas,matplotlib

---
+++ numpi.py
@@ -0,0 +1,23 @@
+# import numpy as np
+# arr = np.array([[10, 20], [30, 40], 50])
+# print(arr)
+
+# minimum dimensions
+# import numpy as np
+# a = np.array([1, 2, 3,4,5], ndmin = 2)
+# print(a)
+
+# dtype parameter
+# import numpy as np
+# a = np.array([1, 2, 3], dtype = bool)
+# print(a)
+
+# import numpy as np
+# a = np.arange(5,82,9)
+# a = a.reshape(3,3)
+# print('Original array is:')
+# print(a)
+# print('\n')
+# print('Modified array is:')
+# for x in np.nditer(a):
+#     print(x)
